3533259484da94228e87244e4debb3aa42f6795a
MarlonG <81429150+mgacrama211176@users.noreply.github.com>
2024-11-04 02:56:38 +0000

    Created a basic calculator using replit agent

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello world!");
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let operator = prompt("Enter the operator (+, -, *, /):");
+
+let result;
+
+if (operator === "+") {
+  result = num1 + num2;
+} else if (operator === "-") {
+  result = num1 - num2;
+} else if (operator === "*") {
+  result = num1 * num2;
+} else if (operator === "/") {
+  result = num1 / num2;
+} else {
+  console.log("Invalid operator.");
+}
+
+if (result !== undefined) {
+  console.log("The result is:", result);
+}
